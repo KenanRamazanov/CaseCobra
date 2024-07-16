f74549ef80df8d7efae868d4619e70767412dac2
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-16 17:07:03 +0400

    Adding functionality to MaxWidthWrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,14 @@ export default function Home() {
    <div className="bg-slate-50">
     <section>
       <MaxWidthWrapper
-       className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
-        <div>
-          
+       className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52 flex flex-col">
+        <div className="col-span-2 px-6 lg:px-0 lg:pt-4 ">
+          <div className="relative mx-auto text-center lg:text-left items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+               <img src="/snake-1.png" alt="" className="w-full" />
+              </div>
+              <h1></h1>
+          </div>
         </div>
        </MaxWidthWrapper>
     </section>
--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -9,7 +9,8 @@ const MaxWidthWrapper = (
   return (
     <div
      className=
-     {cn("h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20"
+     {cn(
+        "h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20"
      ,className)}>
     {children}
     </div>
