a8b5fc832540bf7c4fff5fc4f9b4950faaffd576
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-16 17:21:53 +0400

    Adding Icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Check } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -11,9 +12,24 @@ export default function Home() {
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
                 <img src="/snake-1.png" alt="" className="w-full" />
               </div>
-              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl">
-                Your image on a <span>Custom</span>Phone Case
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your image on a <span className="bg-green-600 px-2 text-white">Custom</span>Phone Case
               </h1>
+              <p className='mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap'>
+                Capture your favorite memories with your own,{' '}
+                <span className='font-semibold'>one-of-one</span> phone case.
+                CaseCobra allows you to protect your memories, not just your
+                phone case.
+              </p>
+
+              <ul className='mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start'>
+                <div className="space-y-2">
+                      <li className="flex gap-1.5 items-center text-left">
+                        <Check/>
+                      High-quality, durable material
+                      </li>
+                </div>
+              </ul>
             </div>
           </div>
         </MaxWidthWrapper>
