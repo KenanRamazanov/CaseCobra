5924e1d7eba5dada1e962f9aa2541355510ed2ce
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-17 19:41:06 +0400

    Adding images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,32 +13,62 @@ export default function Home() {
                 <img src="/snake-1.png" alt="" className="w-full" />
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
-                Your image on a <span className="bg-green-600 px-2 text-white">Custom</span>Phone Case
+                Your image on a{" "}
+                <span className="bg-green-600 px-2 text-white">Custom</span>
+                Phone Case
               </h1>
-              <p className='mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap'>
-                Capture your favorite memories with your own,{' '}
-                <span className='font-semibold'>one-of-one</span> phone case.
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Capture your favorite memories with your own,{" "}
+                <span className="font-semibold">one-of-one</span> phone case.
                 CaseCobra allows you to protect your memories, not just your
                 phone case.
               </p>
 
-              <ul className='mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start'>
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
                 <div className="space-y-2">
-                      <li className="flex gap-1.5 items-center text-left">
-                      <Check className='h-5 w-5 shrink-0 text-green-600' />
-                      High-quality, durable material
-                      </li>
-                      <li className="flex gap-1.5 items-center text-left">
-                      <Check className='h-5 w-5 shrink-0 text-green-600' />
-                      5 year print guarantee
-                      </li>
-                      <li className="flex gap-1.5 items-center text-left">
-                      <Check className='h-5 w-5 shrink-0 text-green-600' />
-                      Modern iPhone models supported
-
-                      </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    High-quality, durable material
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />5 year
+                    print guarantee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    Modern iPhone models supported
+                  </li>
                 </div>
               </ul>
+              <div className='mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5'>
+                <div className="flex -space-x-4">
+                <img
+                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-1.png'
+                    alt='user image'
+                  />
+                  <img
+                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-2.png'
+                    alt='user image'
+                  />
+                  <img
+                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-3.png'
+                    alt='user image'
+                  />
+                  <img
+                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-4.jpg'
+                    alt='user image'
+                  />
+                  <img
+                    className='inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100'
+                    src='/users/user-5.jpg'
+                    alt='user image'
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
