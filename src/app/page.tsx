ddd5b7f7bbd30046a33f52e8abc45d9b210e3fbf
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-18 19:17:56 +0400

    Editing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,44 +40,43 @@ export default function Home() {
                   </li>
                 </div>
               </ul>
-              <div className='mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5'>
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
                 <div className="flex -space-x-4">
-                <img
-                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
-                    src='/users/user-1.png'
-                    alt='user image'
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-1.png"
+                    alt="user image"
                   />
                   <img
-                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
-                    src='/users/user-2.png'
-                    alt='user image'
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-2.png"
+                    alt="user image"
                   />
                   <img
-                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
-                    src='/users/user-3.png'
-                    alt='user image'
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-3.png"
+                    alt="user image"
                   />
                   <img
-                    className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
-                    src='/users/user-4.jpg'
-                    alt='user image'
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-4.jpg"
+                    alt="user image"
                   />
                   <img
-                    className='inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100'
-                    src='/users/user-5.jpg'
-                    alt='user image'
+                    className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-5.jpg"
+                    alt="user image"
                   />
                 </div>
 
-                <div className='flex flex-col justify-between items-center sm:items-start'>
-                 <div className="flex gap-0.5">
-                  <Star className="h-4 w-4 text-green-600 fill-green-600"/>
-                  <Star className="h-4 w-4 text-green-600 fill-green-600"/>
-                  <Star className="h-4 w-4 text-green-600 fill-green-600"/>
-                  <Star className="h-4 w-4 text-green-600 fill-green-600"/>
-                  <Star className="h-4 w-4 text-green-600 fill-green-600"/>
-
-                 </div>
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                  </div>
                 </div>
               </div>
             </div>
