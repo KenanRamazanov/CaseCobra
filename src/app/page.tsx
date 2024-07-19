2631294559e93b07afaf78c139822a384da9e237
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-19 20:24:39 +0400

    Editing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,6 +77,9 @@ export default function Home() {
                     <Star className="h-4 w-4 text-green-600 fill-green-600" />
                     <Star className="h-4 w-4 text-green-600 fill-green-600" />
                   </div>
+                  <p>
+                    <span className='font-semibold'>1.250</span> happy customers
+                  </p>
                 </div>
               </div>
             </div>
