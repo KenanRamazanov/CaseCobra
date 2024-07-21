e910d084b4230406dabcd0451651132a9957bd97
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-21 20:10:46 +0400

    Editing Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import MaxWidthWrapper from "./MaxWidthWrapper"
 import Link from "next/link"
 
 const Navbar = () => {
-
+    const user = undefined
 return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
         <MaxWidthWrapper>
@@ -11,6 +11,19 @@ return (
           <Link href="/" className="flex z-40 font-semibold">
            case <span className="text-green-600">cobra</span>
           </Link>
+
+
+
+          <div className="h-full flex items-center space-x-4">
+           {user ? (
+            <>
+            <Link href='/api/auth/logout'
+            className={}
+            >
+            </Link>
+            </>
+           ) : ()}
+          </div>
             </div>
         </MaxWidthWrapper>
     </nav>
