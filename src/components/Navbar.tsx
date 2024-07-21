c58d86daf64fa91c0076bc9859c65a8d61904286
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-21 20:15:35 +0400

    Editing Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import Link from "next/link"
+import { buttonVariants } from "./ui/button"
 
 const Navbar = () => {
     const user = undefined
+    const isAdmin= false
 return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
         <MaxWidthWrapper>
@@ -18,11 +20,25 @@ return (
            {user ? (
             <>
             <Link href='/api/auth/logout'
-            className={}
-            >
+             className={buttonVariants({
+                size: 'sm',
+                variant: 'ghost',
+              })}>
+              Sign out
             </Link>
+         {isAdmin ? (   <Link href='/api/auth/logout'
+             className={buttonVariants({
+                size: 'sm',
+                variant: 'ghost',
+              })}>
+              Sign out
+            </Link>
+            ) : null}
             </>
-           ) : ()}
+           ) : (
+            <>
+            </>
+           )}
           </div>
             </div>
         </MaxWidthWrapper>
