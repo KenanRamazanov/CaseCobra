672266ee4ed289a2b2cb8bbbfee5e1b95f89197b
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-23 19:08:04 +0400

    Adding new users to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,19 +158,48 @@ export default function Home() {
                 </div>
               </div>
             </div>
-          
-           {/* second user review */}
-           <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
-           <div className='flex gap-0.5 mb-2'>
-                <Star className='h-5 w-5 text-green-600 fill-green-600' />
-                <Star className='h-5 w-5 text-green-600 fill-green-600' />
-                <Star className='h-5 w-5 text-green-600 fill-green-600' />
-                <Star className='h-5 w-5 text-green-600 fill-green-600' />
-                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+
+            {/* second user review */}
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
               </div>
-           </div>
+
+              <div className="text-lg leading-8">
+                  <p>
+                  "I usually keep my phone together with my keys in my pocket
+                  and that led to some pretty heavy scratchmarks on all of my
+                  last phone cases. This one, besides a barely noticeable
+                  scratch on the corner,{' '}
+                  <span className='p-0.5 bg-slate-800 text-white'>
+                  looks brand new after about half a year
+                  </span>
+                  . I dig it."
+                  </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+              <img
+                  className='rounded-full h-12 w-12 object-cover'
+                  src='/users/user-4.jpg'
+                  alt='user'
+                />
+                <div className="flex flex-col">
+                <p className='font-semibold'>Josh</p>
+                <div className='flex gap-1.5 items-center text-zinc-600'>
+                <Check className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <p className='text-sm'>Verified Purchase</p>
+                </div>
+                </div>
+              </div>
+            </div>
           </div>
         </MaxWidthWrapper>
+
+        
       </section>
     </div>
   );
