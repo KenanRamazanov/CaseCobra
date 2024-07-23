a26ea2e7ba2ba5824c1374a5297780dbd14f63de
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-23 19:02:25 +0400

    Editing main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -149,8 +149,26 @@ export default function Home() {
                   src='/users/user-1.png'
                   alt='user'
                 />
+                <div className="flex flex-col">
+                <p className='font-semibold'>Jonathan</p>
+                <div className='flex gap-1.5 items-center text-zinc-600'>
+                <Check className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <p className='text-sm'>Verified Purchase</p>
+                </div>
+                </div>
               </div>
             </div>
+          
+           {/* second user review */}
+           <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+           <div className='flex gap-0.5 mb-2'>
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+              </div>
+           </div>
           </div>
         </MaxWidthWrapper>
       </section>
