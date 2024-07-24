3d2847155a8103735a605185f95eb0f2efb459bd
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-24 20:25:22 +0400

    Adding Functionality to Reviews

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,38 +1,52 @@
-"use client"
+"use client";
 
-import { useRef } from "react";
+import { HTMLAttributes, useEffect, useRef, useState } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { useInView } from "framer-motion";
 
-
 const PHONES = [
-    '/testimonials/1.jpg',
-    '/testimonials/2.jpg',
-    '/testimonials/3.jpg',
-    '/testimonials/4.jpg',
-    '/testimonials/5.jpg',
-    '/testimonials/6.jpg',
-  ]
+  "/testimonials/1.jpg",
+  "/testimonials/2.jpg",
+  "/testimonials/3.jpg",
+  "/testimonials/4.jpg",
+  "/testimonials/5.jpg",
+  "/testimonials/6.jpg",
+];
 
-function ReviewGrid() {
-    const containerRef = useRef<HTMLDivElement | null>(null)
-    const isInView = useInView(containerRef, { once: true, amount: 0.4 })
-  const columns = splitArray(PHONES, 3)
-  const column1 = columns[0]
-  const column2 = columns[1]
-  const column3 = splitArray(columns[2], 2)
-return (
+function splitArray<T>(array: Array<T>, numParts: number) {
+  const result: Array<Array<T>> = [];
 
-    <div
-    ref={containerRef}
-    className='relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3'
+  for (let i = 0; i < array.length; i++) {
+    const index = i % numParts;
+    if (!result[index]) {
+      result[index] = [];
+    }
+    result[index].push(array[i]);
+  }
 
-    >
-
-    </div>
-)
+  return result;
 }
-
+function ReviewGrid() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(containerRef, { once: true, amount: 0.4 });
+  const columns = splitArray(PHONES, 3);
+  const column1 = columns[0];
+  const column2 = columns[1];
+  const column3 = splitArray(columns[2], 2);
+  return (
+    <div
+      ref={containerRef}
+      className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
+    >
+      {isInView ? <>
+      {/* <ReviewColumn
+      
+      /> */}
+      
+      </> : null}
+    </div>
+  );
+}
 
 export function Reviews() {
   return (
