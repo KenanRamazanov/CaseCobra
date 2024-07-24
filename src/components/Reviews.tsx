850b0a284ce5b614e191b5ee7d1b3d21aec4074c
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-24 19:45:12 +0400

    Adding Functionality to Reviews

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,4 +1,23 @@
+"use client"
+
+import { useRef } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+
+
+function ReviewGrid() {
+    const containerRef = useRef<HTMLDivElement | null>(null)
+return (
+
+    <div
+    ref={containerRef}
+    className='relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3'
+    
+    >
+
+    </div>
+)
+}
+
 
 export function Reviews() {
   return (
