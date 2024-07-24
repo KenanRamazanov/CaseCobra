bda8a090b2adf74014e828b91e1995a7e6e2ad9f
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-24 19:40:04 +0400

    Editing Reviews

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
+import { Reviews } from "@/components/Reviews";
 import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
@@ -199,7 +200,9 @@ export default function Home() {
           </div>
         </MaxWidthWrapper>
 
-        <div className="pt-18">  </div>
+        <div className="pt-18">
+          <Reviews/>
+            </div>
       </section>
     </div>
   );
