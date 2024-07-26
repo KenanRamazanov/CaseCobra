b43998bbf8bc33ccc7c644538a22d11a1fa3a1c7
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-26 22:12:38 +0400

    Editing Reviews

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -147,6 +147,8 @@ function ReviewGrid() {
           />
         </>
       ) : null}
+        <div className='pointer-events-none absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-slate-100' />
+      <div className='pointer-events-none absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-slate-100' />
     </div>
   );
 }
