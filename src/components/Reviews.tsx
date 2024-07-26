f7efaf23618b4876ac123704067e65de9feea158
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-26 21:58:40 +0400

    Adding Functionality to Reviews

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -29,49 +29,84 @@ function splitArray<T>(array: Array<T>, numParts: number) {
 }
 
 function ReviewColumn({
-    reviews,
-    className,
-    reviewClassName,
-    msPerPixel = 0,
-  }: {
-    reviews: string[]
-    className?: string
-    reviewClassName?: (reviewIndex: number) => string
-    msPerPixel?: number
-  }) {
-    const columnRef = useRef<HTMLDivElement | null>(null)
-    const [columnHeight, setColumnHeight] = useState(0)
-  const duration = `${columnHeight * msPerPixel}ms`
-  
+  reviews,
+  className,
+  reviewClassName,
+  msPerPixel = 0,
+}: {
+  reviews: string[];
+  className?: string;
+  reviewClassName?: (reviewIndex: number) => string;
+  msPerPixel?: number;
+}) {
+  const columnRef = useRef<HTMLDivElement | null>(null);
+  const [columnHeight, setColumnHeight] = useState(0);
+  const duration = `${columnHeight * msPerPixel}ms`;
+
   useEffect(() => {
-    if (!columnRef.current) return
+    if (!columnRef.current) return;
 
     const resizeObserver = new window.ResizeObserver(() => {
-      setColumnHeight(columnRef.current?.offsetHeight ?? 0)
-    })
+      setColumnHeight(columnRef.current?.offsetHeight ?? 0);
+    });
 
-    resizeObserver.observe(columnRef.current)
+    resizeObserver.observe(columnRef.current);
 
     return () => {
-      resizeObserver.disconnect()
-    }
-  }, [])
+      resizeObserver.disconnect();
+    };
+  }, []);
 
-    return (
-      <div
-        ref={columnRef}
-        className={cn('animate-marquee space-y-8 py-4', className)}
-        style={{ '--marquee-duration': duration } as React.CSSProperties}>
-        {reviews.concat(reviews).map((imgSrc, reviewIndex) => (
-          <Review
-            key={reviewIndex}
-            className={reviewClassName?.(reviewIndex % reviews.length)}
-            imgSrc={imgSrc}
-          />
-        ))}
-      </div>
-    )
-  }
+  return (
+    <div
+      ref={columnRef}
+      className={cn("animate-marquee space-y-8 py-4", className)}
+      style={{ "--marquee-duration": duration } as React.CSSProperties}
+    >
+      {reviews.concat(reviews).map((imgSrc, reviewIndex) => (
+        <Review
+          key={reviewIndex}
+          className={reviewClassName?.(reviewIndex % reviews.length)}
+          imgSrc={imgSrc}
+        />
+      ))}
+    </div>
+  );
+}
+
+interface ReviewProps extends HTMLAttributes<HTMLDivElement>{
+  imgSrc: string
+}
+
+function Review({ imgSrc, className, ...props }: ReviewProps) {
+  const POSSIBLE_ANIMATION_DELAYS = [
+    '0s',
+    '0.1s',
+    '0.2s',
+    '0.3s',
+    '0.4s',
+    '0.5s',
+  ]
+
+  const animationDelay =
+    POSSIBLE_ANIMATION_DELAYS[
+      Math.floor(Math.random() * POSSIBLE_ANIMATION_DELAYS.length)
+    ]
+
+  return (
+    <div
+      className={cn(
+        'animate-fade-in rounded-[2.25rem] bg-white p-6 opacity-0 shadow-xl shadow-slate-900/5',
+        className
+      )}
+      style={{ animationDelay }}
+      {...props}>
+      <Phone imgSrc={imgSrc} />
+    </div>
+  )
+}
+
+
 
 function ReviewGrid() {
   const containerRef = useRef<HTMLDivElement | null>(null);
@@ -85,19 +120,20 @@ function ReviewGrid() {
       ref={containerRef}
       className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
     >
-      {isInView ? <>
-      <ReviewColumn
-      reviews={[...column1, ...column3.flat(), ...column2]}
-      reviewClassName={(reviewIndex) =>
-        cn({
-          'md:hidden': reviewIndex >= column1.length + column3[0].length,
-          'lg:hidden': reviewIndex >= column1.length,
-        })
-      }
-      msPerPixel={10}
-      />
-      
-      </> : null}
+      {isInView ? (
+        <>
+          <ReviewColumn
+            reviews={[...column1, ...column3.flat(), ...column2]}
+            reviewClassName={(reviewIndex) =>
+              cn({
+                "md:hidden": reviewIndex >= column1.length + column3[0].length,
+                "lg:hidden": reviewIndex >= column1.length,
+              })
+            }
+            msPerPixel={10}
+          />
+        </>
+      ) : null}
     </div>
   );
 }
