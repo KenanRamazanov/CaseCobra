158176ea2da56e24b06b094145ee7aab67202905
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-27 21:04:25 +0400

    Editing main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,28 +134,28 @@ export default function Home() {
               </div>
 
               <div className="text-lg leading-8">
-                  <p>
+                <p>
                   "The case feels durable and I even got a compliment on the
-                  design. Had the case for two and a half months now and{' '}
-                  <span className='p-0.5 bg-slate-800 text-white'>
+                  design. Had the case for two and a half months now and{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
                     the image is super clear
                   </span>
                   , on the case I had before, the image started fading into
                   yellow-ish color after a couple weeks. Love it."
-                  </p>
+                </p>
               </div>
               <div className="flex gap-4 mt-2">
-              <img
-                  className='rounded-full h-12 w-12 object-cover'
-                  src='/users/user-1.png'
-                  alt='user'
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-1.png"
+                  alt="user"
                 />
                 <div className="flex flex-col">
-                <p className='font-semibold'>Jonathan</p>
-                <div className='flex gap-1.5 items-center text-zinc-600'>
-                <Check className='h-4 w-4 stroke-[3px] text-green-600' />
-                    <p className='text-sm'>Verified Purchase</p>
-                </div>
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -171,29 +171,29 @@ export default function Home() {
               </div>
 
               <div className="text-lg leading-8">
-                  <p>
+                <p>
                   "I usually keep my phone together with my keys in my pocket
                   and that led to some pretty heavy scratchmarks on all of my
                   last phone cases. This one, besides a barely noticeable
-                  scratch on the corner,{' '}
-                  <span className='p-0.5 bg-slate-800 text-white'>
-                  looks brand new after about half a year
+                  scratch on the corner,{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    looks brand new after about half a year
                   </span>
                   . I dig it."
-                  </p>
+                </p>
               </div>
               <div className="flex gap-4 mt-2">
-              <img
-                  className='rounded-full h-12 w-12 object-cover'
-                  src='/users/user-4.jpg'
-                  alt='user'
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-4.jpg"
+                  alt="user"
                 />
                 <div className="flex flex-col">
-                <p className='font-semibold'>Josh</p>
-                <div className='flex gap-1.5 items-center text-zinc-600'>
-                <Check className='h-4 w-4 stroke-[3px] text-green-600' />
-                    <p className='text-sm'>Verified Purchase</p>
-                </div>
+                  <p className="font-semibold">Josh</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -201,8 +201,12 @@ export default function Home() {
         </MaxWidthWrapper>
 
         <div className="pt-18">
-          <Reviews/>
-            </div>
+          <Reviews />
+        </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24"></MaxWidthWrapper>
       </section>
     </div>
   );
