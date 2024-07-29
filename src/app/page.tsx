8697ec26dd1247a8ba47c0c7b0010b2cfc10102b
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-29 12:09:41 +0400

    Final of main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
 import { Reviews } from "@/components/Reviews";
-import { Check, Star } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowRight, Check, Star } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -220,42 +222,53 @@ export default function Home() {
           </div>
 
           <div className="mx-auto max-w-6xl px-6 lg:px-8">
-          <div className='relative flex flex-col items-center md:grid grid-cols-2 gap-40'>
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
               <img
-                src='/arrow.png'
-                className='absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0'
+                src="/arrow.png"
+                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
               />
 
-              <div className='relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl'>
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
                 <img
-                  src='/horse.jpg'
-                  className='rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full'
+                  src="/horse.jpg"
+                  className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
                 />
               </div>
 
-              <Phone className='w-60' imgSrc='/horse_phone.jpg' />
+              <Phone className="w-60" imgSrc="/horse_phone.jpg" />
             </div>
           </div>
 
-
           <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
-           <li className="w-fit ">
-            <Check className="h-5 w-5 text-green-600 inline mr-1.5"/>
-            High-quality silicone material
-           </li>
-           <li className="w-fit ">
-            <Check className="h-5 w-5 text-green-600 inline mr-1.5"/>
-            Scratch- and fingerprint resistant coating
-           </li>
-           <li className="w-fit ">
-            <Check className="h-5 w-5 text-green-600 inline mr-1.5"/>
-            Wireless charging compatible
-           </li>
+            <li className="w-fit ">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              High-quality silicone material
+            </li>
+            <li className="w-fit ">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              Scratch- and fingerprint resistant coating
+            </li>
+            <li className="w-fit ">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              Wireless charging compatible
+            </li>
 
-           <li className="w-fit ">
-            <Check className="h-5 w-5 text-green-600 inline mr-1.5"/>
-            5 year print warranty
-           </li>
+            <li className="w-fit ">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />5 year
+              print warranty
+            </li>
+
+            <div className="flex justify-center">
+              <Link
+                className={buttonVariants({
+                  size: "lg",
+                  className: "mx-auto mt-8",
+                })}
+                href="/configure/upload"
+              >
+                Create your case now <ArrowRight className="h-4 w-4 ml-1.5" />
+              </Link>
+            </div>
           </ul>
         </MaxWidthWrapper>
       </section>
