d263cd1eefbabe4ca83601e73db6d4064cc83a51
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-29 11:56:36 +0400

    Editing main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -207,8 +207,16 @@ export default function Home() {
 
       <section>
         <MaxWidthWrapper className="py-24">
-          <div className="flex">
-
+          <div className="mb-12 px-6 lg:px-8">
+             <div className="mx-auto max-w-2xl sm:text-center">
+             <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
+                Upload your photo and get{' '}
+                <span className='relative px-2 bg-green-600 text-white'>
+                  your own case
+                </span>{' '}
+                now
+              </h2>
+             </div>
           </div>
         </MaxWidthWrapper>
       </section>
