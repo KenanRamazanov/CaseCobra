daf2dad00826fa8bf3b65022e33804e159c41b02
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-29 12:00:24 +0400

    Adding functionality to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -208,15 +208,33 @@ export default function Home() {
       <section>
         <MaxWidthWrapper className="py-24">
           <div className="mb-12 px-6 lg:px-8">
-             <div className="mx-auto max-w-2xl sm:text-center">
-             <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
-                Upload your photo and get{' '}
-                <span className='relative px-2 bg-green-600 text-white'>
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                Upload your photo and get{" "}
+                <span className="relative px-2 bg-green-600 text-white">
                   your own case
-                </span>{' '}
+                </span>{" "}
                 now
               </h2>
-             </div>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+          <div className='relative flex flex-col items-center md:grid grid-cols-2 gap-40'>
+              <img
+                src='/arrow.png'
+                className='absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0'
+              />
+
+              <div className='relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl'>
+                <img
+                  src='/horse.jpg'
+                  className='rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full'
+                />
+              </div>
+
+              <Phone className='w-60' imgSrc='/horse_phone.jpg' />
+            </div>
           </div>
         </MaxWidthWrapper>
       </section>
