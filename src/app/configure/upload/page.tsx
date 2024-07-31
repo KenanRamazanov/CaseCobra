cfdb34177401ab94a8ebe6654db208034034f1de
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-31 10:17:42 +0400

    Adding functionality to Upload page

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,25 +1,46 @@
-"use client"
-import { cn } from '@/lib/utils'
-import React, { useState } from 'react'
+"use client";
+import { cn } from "@/lib/utils";
+import React, { useState } from "react";
+import Dropzone, { FileRejection } from "react-dropzone";
 
 const Page = () => {
-    const [isDragOver, setIsDragOver] = useState<boolean>(false)
+  const [isDragOver, setIsDragOver] = useState<boolean>(false);
+
+  const onDropRejected = () => {};
+  const onDropAccepted = () => {};
+
   return (
     <div
-    className={cn(
-        'relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center',
+      className={cn(
+        "relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center",
         {
-          'ring-blue-900/25 bg-blue-900/10': isDragOver,
+          "ring-blue-900/25 bg-blue-900/10": isDragOver,
         }
-    )}
+      )}
     >
+      <div className="relative flex flex-1 flex-col items-center justify-center w-full">
+        <Dropzone
+          onDropRejected={onDropAccepted}
+          onDropAccepted={onDropRejected}
+          accept={{
+            "image/png": [".png"],
+            "image/jpeg": [".jpeg"],
+            "image/jpg": [".jpg"],
+          }}
+          onDragEnter={() => setIsDragOver(true)}
+          onDragLeave={() => setIsDragOver(false)}
+        >
+          {({getRootProps ,getInputProps}) => (
+              <div className="h-full w-full flex-1 flex flex-col items-center justify-center"
+              {...getRootProps()}
+              >
 
-        <div className='relative flex flex-1 flex-col items-center justify-center w-full'>
-          
-        </div>
-
+              </div>
+          )}
+        </Dropzone>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
