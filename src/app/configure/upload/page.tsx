d3197f77a6fe37cdf31a3162b4b11df36b08cde6
kenan551 <kenan.ramazanov1104@gmail.com>
2024-08-03 20:25:38 +0400

    Editing Upload

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -46,7 +46,15 @@ const Page = () => {
                 <Image className=" h-6 w-6  text-zinc-500 mb-2" />
               )}
               <div className="flex flex-col justify-center mb-2 text-sm text-zinc-700">
-
+                {isUploading ? (
+                  <div></div>
+                ) : isPending ? (
+                  <div></div>
+                ) : isDragOver ? (
+                  <span></span>
+                ) : (
+                  <span></span>
+                )}
               </div>
             </div>
           )}
