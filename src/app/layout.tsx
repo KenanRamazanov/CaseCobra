431bfdcc91fc8fd2c15945fa0cfd57bd895651c7
kenan551 <kenan.ramazanov1104@gmail.com>
2024-08-06 11:48:40 +0400

    Editing layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,9 @@ export default function RootLayout({
           {children}
           </div>
         </main>
+
+
+        <Toaster/>
         <Footer/>
         </body>
     </html>
