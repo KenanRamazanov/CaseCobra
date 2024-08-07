9bbe6ce2b4ba962d27eafe056897c9429101a9f4
kenan551 <kenan.ramazanov1104@gmail.com>
2024-08-07 19:10:41 +0400

    Editing core api

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,7 +1,6 @@
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
 import { z } from 'zod'
-import sharp from 'sharp'
-import { db } from '@/db'
+
 
 const f = createUploadthing()
 
@@ -12,37 +11,11 @@ export const ourFileRouter = {
       return { input }
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      const { configId } = metadata.input
-
-      const res = await fetch(file.url)
-      const buffer = await res.arrayBuffer()
-
-      const imgMetadata = await sharp(buffer).metadata()
-      const { width, height } = imgMetadata
-
-      if (!configId) {
-        const configuration = await db.configuration.create({
-          data: {
-            imageUrl: file.url,
-            height: height || 500,
-            width: width || 500,
-          },
+        const {configId} = metadata.input
+      return {configId}
         })
 
-        return { configId: configuration.id }
-      } else {
-        const updatedConfiguration = await db.configuration.update({
-          where: {
-            id: configId,
-          },
-          data: {
-            croppedImageUrl: file.url,
-          },
-        })
-
-        return { configId: updatedConfiguration.id }
-      }
-    }),
+       
 } satisfies FileRouter
 
 export type OurFileRouter = typeof ourFileRouter
--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Progress } from "@/components/ui/progress";
+import { useToast } from "@/components/ui/use-toast";
 import { useUploadThing } from "@/lib/uploadthing";
 import { cn } from "@/lib/utils";
 import { Image, Loader2, MousePointerSquareDashed } from "lucide-react";
@@ -8,12 +9,13 @@ import React, { useState, useTransition } from "react";
 import Dropzone, { FileRejection } from "react-dropzone";
 
 const Page = () => {
+  const {toast} = useToast();
   const [isDragOver, setIsDragOver] = useState<boolean>(false);
-  const isUploading = false;
+
   const [isPending, startTransition] = useTransition();
 const router = useRouter()
 
-  const { startUpload} = useUploadThing('imageUploader', {
+  const { startUpload,isUploading} = useUploadThing('imageUploader', {
     onClientUploadComplete: ([data]) => {
       const configId = data.serverData.configId
       startTransition(() => {
@@ -29,7 +31,12 @@ const router = useRouter()
     const [file] = rejectedFiles
 
     setIsDragOver(false)
-    
+
+    toast({
+      title: `${file.file.type} type is not supported.`,
+      description: "Please choose a PNG, JPG, or JPEG image instead.",
+      variant: "destructive"
+    })
 
   };
   const onDropAccepted = (acceptedFiles: File[]) => {
