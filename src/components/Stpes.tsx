7290ae54e910909605daa3452069173b202f5085
kenan551 <kenan.ramazanov1104@gmail.com>
2024-08-11 08:50:52 +0400

    Editing Stpes

--- src/components/Stpes.tsx
+++ src/components/Stpes.tsx
@@ -2,6 +2,25 @@
 
 import { usePathname } from "next/navigation"
 
+
+const STEPS = [
+    {
+        name: 'Step 1: Add image',
+        description: 'Choose an image for your case',
+        url: '/upload',
+      },
+      {
+        name: 'Step 2: Customize design',
+        description: 'Make the case yours',
+        url: '/design',
+      },
+      {
+        name: 'Step 3: Summary',
+        description: 'Review your final design',
+        url: '/preview',
+      },
+]
+
 const Stpes = () => {
     const pathname = usePathname()
   return (
