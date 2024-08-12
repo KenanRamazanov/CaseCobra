d23582ab12b145a8ebcabe5d0bc2842ef93b7ab7
kenan551 <kenan.ramazanov1104@gmail.com>
2024-08-12 23:17:18 +0400

    Editing Steps

--- src/components/Stpes.tsx
+++ src/components/Stpes.tsx
@@ -1,31 +1,40 @@
-"use client"
+"use client";
 
-import { usePathname } from "next/navigation"
-
+import { usePathname } from "next/navigation";
 
 const STEPS = [
-    {
-        name: 'Step 1: Add image',
-        description: 'Choose an image for your case',
-        url: '/upload',
-      },
-      {
-        name: 'Step 2: Customize design',
-        description: 'Make the case yours',
-        url: '/design',
-      },
-      {
-        name: 'Step 3: Summary',
-        description: 'Review your final design',
-        url: '/preview',
-      },
-]
+  {
+    name: "Step 1: Add image",
+    description: "Choose an image for your case",
+    url: "/upload",
+  },
+  {
+    name: "Step 2: Customize design",
+    description: "Make the case yours",
+    url: "/design",
+  },
+  {
+    name: "Step 3: Summary",
+    description: "Review your final design",
+    url: "/preview",
+  },
+];
 
 const Stpes = () => {
-    const pathname = usePathname()
+  const pathname = usePathname();
   return (
-    <div>Stpes</div>
-  )
-}
+    <ol className="rounded-md bg-white lg:flex lg:rounded-none lg:border-l lg:border-r lg:border-gray-200">
+      {STEPS.map((step, i) => {
+        const isCurrent = pathname.endsWith(step.url);
+        const isCompleted = STEPS.slice(i + 1).some((step) =>
+          pathname.endsWith(step.url)
+        );
 
-export default Stpes
+        const imgPath = `/snake-${i + 1}.png`
+        return <li></li>;
+      })}
+    </ol>
+  );
+};
+
+export default Stpes;
