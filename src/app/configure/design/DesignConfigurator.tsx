477ef5dfcd056a023144c9aaeec26cd87c196e6a
kenan551 <kenan.ramazanov1104@gmail.com>
2024-08-17 12:07:49 +0400

    Creating AspectRatio

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,3 +1,4 @@
+import { AspectRatio } from '@/components/ui/aspect-ratio';
 import React from 'react'
 
 
@@ -9,7 +10,17 @@ interface DesignConfiguratorProps {
 
 const DesignConfigurator = ({configId,imageUrl,imageDimensions}: DesignConfiguratorProps) => {
   return (
-    <div>DesignConfigurator</div>
+    <div className='relative mt-20 grid grid-cols-3 mb-20 pb-20'>
+      <div className='relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2'>
+      <div className='relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]'>
+          <AspectRatio
+          ratio={896 / 1831}
+          className='pointer-events-none relative z-50 aspect-[896/1831] w-full'>
+            
+          </AspectRatio>
+         </div>
+      </div>
+      </div>
   )
 }
 
