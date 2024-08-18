2badfc73a8f5db04a32279424e273f5a5aeffbf5
kenan551 <kenan.ramazanov1104@gmail.com>
2024-08-18 12:24:50 +0400

    Editing DesignConfigurator

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,6 +1,7 @@
 import { AspectRatio } from '@/components/ui/aspect-ratio';
 import React from 'react'
-
+import NextImage from 'next/image'
+import { cn } from '@/lib/utils';
 
 interface DesignConfiguratorProps {
   configId: string
@@ -16,8 +17,29 @@ const DesignConfigurator = ({configId,imageUrl,imageDimensions}: DesignConfigura
           <AspectRatio
           ratio={896 / 1831}
           className='pointer-events-none relative z-50 aspect-[896/1831] w-full'>
-            
+            <NextImage
+              fill
+              alt='phone image'
+              src='/phone-template.png'
+              className='pointer-events-none z-50 select-none'
+            />
           </AspectRatio>
+          <div className='absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]' />
+          <div
+            className={cn(
+              'absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]',
+              `bg-blue-950`
+            )}
+          />
+         </div>
+
+         <div className='relative w-full h-full'>
+          <NextImage
+           src={imageUrl}
+           fill
+           alt='your image'
+           className='pointer-events-none'
+          />
          </div>
       </div>
       </div>
